cb43046f26c6832e76b8dc1d3f67d432475123df
Sohel Rana <srana@ael-bd.com>
2023-08-08 17:37:14 +0600

    Create UserPhysicalController.cs

---
+++ RecruiterPortal.API/Controllers/UserPhysicalController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecruiterPortal.API.Controllers;
+using RecruiterPortal.DAL.SqlModels;
+using RecruiterPortalDAL.Managers;
+using RecruiterPortalDAL.Models;
+
+namespace ApplicantPortalAPI.ResourceServer.Controllers
+{
+    [Route("api/user-physical")]
+    [ApiController]
+    public class UserPhysicalController : CustomControllerBase
+    {
+        [Route("get/{applicantId}")]
+        [HttpGet]
+        public IActionResult GetUserPhysicalInfoByUserId(int applicantId)
+        {
+
+            try
+            {
+                UserPhysical userPhysical = UserPhysicalManager.GetByUserID(applicantId);
+                UserPhysicalModel userPhysicalModel = null;
+
+                if (userPhysical != null)
+                {
+                    userPhysicalModel = new UserPhysicalModel();
+                    userPhysicalModel.UserPhysicalID = userPhysical.UserPhysicalId;
+                    userPhysicalModel.Height = userPhysical.Height;
+                    userPhysicalModel.HairColor = userPhysical.HairColor;
+                    userPhysicalModel.EyeColor = userPhysical.EyeColor;
+                    userPhysicalModel.Race = userPhysical.Race;
+                    userPhysicalModel.Weight = userPhysical.Weight;
+                    userPhysicalModel.UserID = userPhysical.UserId;
+                }
+
+                return Ok(userPhysicalModel);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+            return BadRequest();
+        }
+
+        [Route("save")]
+        [HttpPost]
+        public IActionResult Save(UserPhysicalModel userPhysicalModel)
+        {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    UserPhysical userPhysical = new UserPhysical();
+                    userPhysical.UserPhysicalId = userPhysicalModel.UserPhysicalID;
+                    userPhysical.Height = userPhysicalModel.Height;
+                    userPhysical.HairColor = userPhysicalModel.HairColor;
+                    userPhysical.EyeColor = userPhysicalModel.EyeColor;
+                    userPhysical.Race = userPhysicalModel.Race;
+                    userPhysical.Weight = userPhysicalModel.Weight;
+                    userPhysical.UserId = userPhysicalModel.UserID;
+
+                    UserPhysical isExist = UserPhysicalManager.GetByUserID(userPhysicalModel.UserID);
+
+                    if (isExist == null)
+                    {
+                        UserPhysicalManager.SaveUserPhysical(userPhysical);
+                    }
+                    else
+                    {
+                        UserPhysicalManager.UpdateUserPhysical(userPhysical);
+                    }
+
+                    return Ok();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+            return BadRequest();
+        }
+    }
+}
