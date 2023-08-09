2c9cf0a2bf531481f9b6b15b507d0a024a532da5
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-09 14:25:36 +0600

    add and read clients

--- RecruiterPortal.Auth/Configurations/ConfigureIdentityServerServices.cs
+++ RecruiterPortal.Auth/Configurations/ConfigureIdentityServerServices.cs
@@ -1,4 +1,6 @@
-﻿using RecruiterPortal.Auth.Extensions;
+﻿using IdentityServer4.Models;
+using Newtonsoft.Json;
+using RecruiterPortal.Auth.Extensions;
 using System.Security.Cryptography.X509Certificates;
 
 namespace RecruiterPortal.Auth.Configurations
@@ -7,14 +9,33 @@ namespace RecruiterPortal.Auth.Configurations
     {
         public static IServiceCollection ConfigureIdentityServerServices(this IServiceCollection services, IConfiguration configuration)
         {
+            //var clients = configuration.GetSection("IdentityServer").GetChildren().Select(x=>x.Value);
+
             services.AddIdentityServer()
             .AddSigningCredential(new X509Certificate2(Path.Combine("idsrv3test.pfx"), "idsrv3test"))
             .AddInMemoryApiResources(configuration.GetSection("IdentityServer:ApiResources"))
             .AddInMemoryApiScopes(configuration.GetSection("IdentityServer:ApiScopes"))
-            .AddInMemoryClients(configuration.GetSection("IdentityServer:Clients"))
+            .AddInMemoryClients(GetClients())
             .AddCustomUserStore();
 
             return services;
         }
+
+        public static List<Client> GetClients() 
+        {
+            List<Client> clients = new List<Client>();
+
+            using (StreamReader reader = new StreamReader("clients.json"))
+            {
+                clients = JsonConvert.DeserializeObject<List<Client>>(reader.ReadToEnd());
+                foreach (dynamic client in clients)
+                {
+                    var corsOrigin = client.AllowedCorsOrigins;
+                }
+            }
+
+            return clients;
+        }
     }
+    
 }
