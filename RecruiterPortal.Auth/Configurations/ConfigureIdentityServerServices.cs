3ec3742a3984629c958ae350c5d6d14e174086ca
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-09 16:33:58 +0600

    read clients from appsettings

--- RecruiterPortal.Auth/Configurations/ConfigureIdentityServerServices.cs
+++ RecruiterPortal.Auth/Configurations/ConfigureIdentityServerServices.cs
@@ -9,33 +9,53 @@ namespace RecruiterPortal.Auth.Configurations
     {
         public static IServiceCollection ConfigureIdentityServerServices(this IServiceCollection services, IConfiguration configuration)
         {
-            //var clients = configuration.GetSection("IdentityServer").GetChildren().Select(x=>x.Value);
-
             services.AddIdentityServer()
             .AddSigningCredential(new X509Certificate2(Path.Combine("idsrv3test.pfx"), "idsrv3test"))
             .AddInMemoryApiResources(configuration.GetSection("IdentityServer:ApiResources"))
             .AddInMemoryApiScopes(configuration.GetSection("IdentityServer:ApiScopes"))
-            .AddInMemoryClients(GetClients())
+            .AddInMemoryClients(GetClients(configuration))
             .AddCustomUserStore();
 
             return services;
         }
 
-        public static List<Client> GetClients() 
+        public static List<Client> GetClients(IConfiguration configuration)
+        {
+            List<Client> clients = configuration.GetSection("IdentityServer:Clients").Get<List<Client>>();                        
+
+            foreach (var client in clients)
+            {
+                List<string> allowedCorsOrigins = new List<string>();
+                allowedCorsOrigins.Add("http://umrtest.com");
+
+                foreach (var corsOrigin in client.AllowedCorsOrigins)
+                {
+                    allowedCorsOrigins.Add(corsOrigin);
+                }
+
+                client.AllowedCorsOrigins = allowedCorsOrigins;
+            }
+            
+
+            return clients;
+        }
+
+        public static List<Client> GetClientsFromJson()
         {
             List<Client> clients = new List<Client>();
 
             using (StreamReader reader = new StreamReader("clients.json"))
             {
                 clients = JsonConvert.DeserializeObject<List<Client>>(reader.ReadToEnd());
+                List<string> allowedCorsOrigins = new List<string>();
                 foreach (dynamic client in clients)
                 {
-                    var corsOrigin = client.AllowedCorsOrigins;
+                    client.AllowedCorsOrigins.Add("Http://localhost:7000");
                 }
             }
 
             return clients;
         }
     }
-    
+
 }
