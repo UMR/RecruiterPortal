e3eff1ef80aa4380ba69f8e3150eee9bfe91148f
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-11 12:23:07 +0600

    add script

---
+++ RecruiterPortal.DB/2023-08-09/04_AZ_02_GetRecruiterByLoginId.sql
@@ -0,0 +1,16 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_GetRecruiterByLoginid]    Script Date: 8/11/2023 12:21:52 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[sp_GetRecruiterByLoginid]
+(
+    @LoginId NVARCHAR(50)
+)
+
+AS
+
+SELECT * FROM [dbo].[Recruiter] WHERE LoginId=@LoginId;
