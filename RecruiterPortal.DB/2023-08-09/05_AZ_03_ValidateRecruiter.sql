aa9735d8071d03687de850e721c3c9cb3af52ee3
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-11 12:25:02 +0600

    add script

---
+++ RecruiterPortal.DB/2023-08-09/05_AZ_03_ValidateRecruiter.sql
@@ -0,0 +1,20 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_ValidateRecruiter]    Script Date: 8/11/2023 12:24:21 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[sp_ValidateRecruiter]  
+   @LoginId NVARCHAR(50), 
+   @Password NVARCHAR(200),
+   @AgencyId BIGINT
+AS 
+BEGIN   
+		SELECT * FROM [Recruiter] WHERE LoginId = @LoginId AND [Password] = @Password AND AgencyId=@AgencyId;
+END
