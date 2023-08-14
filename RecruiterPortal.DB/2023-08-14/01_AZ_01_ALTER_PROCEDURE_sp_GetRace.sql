7d4f67bd58392ba4efc86aec83f0195b2fa06d9a
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-14 13:54:35 +0600

    add scripts

---
+++ RecruiterPortal.DB/2023-08-14/01_AZ_01_ALTER_PROCEDURE_sp_GetRace.sql
@@ -0,0 +1,25 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_GetRace]    Script Date: 8/14/2023 1:50:08 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[sp_GetRace] 
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * FROM Race
+END
+
