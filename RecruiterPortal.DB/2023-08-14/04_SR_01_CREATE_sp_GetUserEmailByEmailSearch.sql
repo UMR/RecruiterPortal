28024a52b1cb706d73355b665c0561df4713829d
Sohel Rana <srana@ael-bd.com>
2023-08-14 16:26:50 +0600

    added

---
+++ RecruiterPortal.DB/2023-08-14/04_SR_01_CREATE_sp_GetUserEmailByEmailSearch.sql
@@ -0,0 +1,22 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_GetUserLicenseByID]    Script Date: 8/14/2023 1:53:56 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[sp_GetUserEmailByEmailSearch]
+	-- Add the parameters for the stored procedure here	
+	@Email NVARCHAR(50)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+    SELECT [Email] FROM [User] WHERE Email Like '%'+ @Email+'%';
+END
