3367b92188ccbbd0c8e64bec07066bd32546c1b0
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-14 14:01:45 +0600

    add script

---
+++ RecruiterPortal.DB/Special/01_Race_Data_Insert.sql
@@ -0,0 +1,11 @@
+﻿
+INSERT INTO [UMRRecruitmentApplicant].[dbo].[Race]
+           ([Race]
+           ,[RaceCode]
+           ,[CreatedBy]
+           ,[CreatedDate]
+           ,[UpdatedBy]
+           ,[UpdatedDate])
+
+SELECT * FROM [UMRRecruitementDB_New].[dbo].[Race]
+     
