1eafca7e55694f64c987c6f275d73f3ed68dd01d
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-15 14:41:27 +0600

    add scripts

---
+++ RecruiterPortal.DB/2023-08-15/02_AZ_02_CREATE_TABLE_Institution.sql
@@ -0,0 +1,25 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Institution](
+	[Id] [int] IDENTITY(1,1) NOT NULL,	
+	[InstituteName] [nvarchar](250) NOT NULL,	
+	[IsActive] [bit] NULL,
+	[CreatedBy] [int] NULL,
+	[CreatedDate] [datetime] NULL,
+	[UpdatedBy] [int] NULL,
+	[UpdatedDate] [datetime] NULL	
+ CONSTRAINT [PK_Institution] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
