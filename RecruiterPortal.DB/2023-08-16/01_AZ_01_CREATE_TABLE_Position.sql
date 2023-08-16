09ee853cc2f3e326b234c72fb9e77b3227e4ba7c
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-16 16:30:13 +0600

    script added

---
+++ RecruiterPortal.DB/2023-08-16/01_AZ_01_CREATE_TABLE_Position.sql
@@ -0,0 +1,29 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+
+/****** Object:  Table [dbo].[Position]    Script Date: 8/16/2023 4:18:07 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE IF EXISTS [dbo].[Position]
+GO
+
+CREATE TABLE [dbo].[Position](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[PositionName] [varchar](100) NOT NULL,
+	[IsActive] [bit] NULL,
+	[CreatedBy] [int] NULL,
+	[CreatedDate] [datetime] NULL,
+	[UpdatedBy] [int] NULL,
+	[UpdatedDate] [datetime] NULL,
+ CONSTRAINT [PK_Position] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
---
+++ RecruiterPortal.DB/2023-08-16/02_AZ_02_ALTER_PROCEDURE_GetPosition.sql
@@ -0,0 +1,21 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_GetInstitution]    Script Date: 8/16/2023 12:54:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[sp_GetPosition] 
+@PositionName VARCHAR(250) NULL
+AS
+BEGIN	
+
+	IF(@PositionName IS NOT NULL)
+		SELECT * FROM [dbo].[Position] WHERE PositionName LIKE  @PositionName + '%' ORDER BY PositionName;
+	ELSE
+	SELECT TOP(100) * FROM [dbo].[Position] ORDER BY PositionName
+
+END
+
+
