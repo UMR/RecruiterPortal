d80224d1d1a57d1442013d19d138b098673708cd
Sohel Rana <srana@ael-bd.com>
2023-08-22 21:03:04 +0600

    role manager

---
+++ RecruiterPortal.API/Controllers/RoleController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecruiterPortal.API.Controllers;
+using RecruiterPortal.DAL.Models;
+using RecruiterPortal.DAL.SqlModels;
+using RecruiterPortalDAL.Managers;
+using System.Data;
+
+namespace ApplicantPortalAPI.ResourceServer.Controllers
+
+{
+    [Route("api/role")]
+    [ApiController]
+    public class RoleController : CustomControllerBase
+    {
+        public RoleController(ILogger<CustomControllerBase> logger) : base(logger)
+        {
+        }
+
+        [Route("get")]
+        [HttpGet]
+        public IActionResult GetRole()
+        {
+            try
+            {
+                IEnumerable<Role> roles = RoleManager.GetRole();
+                List<string> roleList = new List<string>();
+                foreach (Role role in roles)
+                {
+                    roleList.Add(role.RoleName);
+                }
+                return Ok(roleList.ToArray());
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong: {ex}");
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ RecruiterPortal.DAL/Managers/RoleManager.cs
@@ -0,0 +1,24 @@
+﻿using RecruiterPortal.DAL.Repository;
+using RecruiterPortal.DAL.SqlModels;
+
+namespace RecruiterPortalDAL.Managers
+{
+    public class RoleManager
+    {
+        public static IEnumerable<Role> GetRole()
+        {
+            string spName = "sp_GetRole";
+            try
+            {
+                GenericRepository<Role> roleRepo = new GenericRepository<Role>();
+                IEnumerable<Role> roles = roleRepo.GetAll(spName); ;
+
+                return roles;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
