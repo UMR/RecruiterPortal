6da19ac9357af84582129fa075294861dd529ce8
Sohel Rana <srana@ael-bd.com>
2023-08-22 16:45:32 +0600

    sp added

---
+++ RecruiterPortal.DB/2023-08-21/04_SR_02_SP_GetRole.sql
@@ -0,0 +1,17 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_All_GET_Agency]    Script Date: 8/21/2023 5:22:47 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[sp_GetRole]  
+AS 
+BEGIN      
+	SELECT * FROM [Roles];
+END
