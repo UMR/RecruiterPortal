bbcf407e8d3f19c77f6433804690cedcece813cd
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-28 12:45:25 +0600

    add script for zip code

--- RecruiterPortal.DB/2023-08-23/03_AZ_02_ALTER_PROCEDURE_sp_GetZipCodeByZipCode.sql
+++ RecruiterPortal.DB/2023-08-23/03_AZ_02_ALTER_PROCEDURE_sp_GetZipCodeByZipCode.sql
@@ -1,1 +1,26 @@
-﻿
+﻿USE [UMRRecruitmentApplicant]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_GetZipCodeByZipCode]    Script Date: 8/23/2023 2:45:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[sp_GetZipCodeByZipCode] 
+	-- Add the parameters for the stored procedure here
+	@Zipcode nvarchar(10)
+AS
+BEGIN	
+	
+	SET NOCOUNT ON;
+    
+	if (@Zipcode IS NOT NULL)
+		SELECT TOP 10 * FROM View_LookUp_ZipCode WHERE ZipCode LIKE @Zipcode + '%'
+	ELSE
+		SELECT TOP 100 * FROM View_LookUp_ZipCode 
+
+END
