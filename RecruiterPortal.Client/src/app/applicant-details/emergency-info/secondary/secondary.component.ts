d9683b0c3711bae2d8b53c1c55c9fddb0ebf0bbf
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-29 20:02:54 +0600

    show secondary emergency info

--- RecruiterPortal.Client/src/app/applicant-details/emergency-info/secondary/secondary.component.ts
+++ RecruiterPortal.Client/src/app/applicant-details/emergency-info/secondary/secondary.component.ts
@@ -1,77 +1,78 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
-import { Router } from '@angular/router';
 import { HttpResponse } from '@angular/common/http';
 import { EmergencyInfoService } from '../emergency-info.service';
 import { StorageService } from '../../../common/services/storage.service';
 
 @Component({
-  selector: 'app-secondary',
-  templateUrl: './secondary.component.html',
-  styleUrls: ['./secondary.component.css']
+    selector: 'app-secondary',
+    templateUrl: './secondary.component.html',
+    styleUrls: ['./secondary.component.css']
 })
 export class SecondaryComponent implements OnInit {
-  public isLoading: boolean = false;
-  public secondaryInfo: EmergencyInfoModel[] = [];
+    public isLoading: boolean = false;
+    public secondaryInfo: EmergencyInfoModel[] = [];
 
-  constructor(private messageService: MessageService,
-    private emergencyInfoService: EmergencyInfoService,
-    private storageService: StorageService,
-    private router: Router) { }
+    constructor(private messageService: MessageService,
+        private emergencyInfoService: EmergencyInfoService,
+        private storageService: StorageService)
+    { }
 
-  ngOnInit() {
-    this.isLoading = true;
-    this.emergencyInfoService.getEmergencyInfoByApplicantId(this.storageService.getApplicantId)
-      .subscribe(response => {
-        const data = (response as any).Data;        
-        if (data) {
-          for (let i = 0; i < data.length; i++) {
-            if (data[i].EmrType && +data[i].EmrType === 2) {
-              this.secondaryInfo = data[i];
-            }
-          }
-        }
-      },
-        err => {
-          this.isLoading = false;
-          this.messageService.add({ key: 'toastKey1', severity: 'error', summary: 'Failed to load secondary info', detail: '' });
-        },
-        () => {
-          this.isLoading = false;
-        });
-  }
-
-  onDownloadClick() {
-    this.isLoading = true;
-    this.emergencyInfoService.getEmergencyInfoFileByApplicantId(this.storageService.getApplicantId)
-      .subscribe(response => {
-        if (response.status === 200) {
-          const filename = this.getFileName(response)
-          const downloadLink = document.createElement('a');
-          downloadLink.href = window.URL.createObjectURL(response.body);
-          downloadLink.setAttribute('download', filename.trim().replace('"', '').replace('"', ''));
-          document.body.appendChild(downloadLink);
-          downloadLink.click();
-        }
-      },
-        err => {
-          this.isLoading = false;
-          this.messageService.add({ key: 'toastKey1', severity: 'error', summary: 'Failed to get emergency info file', detail: '' });
-        },
-        () => {
-          this.isLoading = false;
-        });
-  }
-
-  getFileName(response: HttpResponse<Blob>) {
-    let filename: string;
-    try {
-      const contentDisposition = response.headers.get('content-disposition');
-      filename = contentDisposition.split(';')[1].split('filename')[1].split('=')[1].trim();
+    ngOnInit() {
+        this.isLoading = true;
+        this.emergencyInfoService.getEmergencyInfoByApplicantId(this.storageService.getApplicantId)
+            .subscribe(response => {
+                if (response.status === 200) {
+                    const data = response.body;
+                    if (data && data.length > 0) {
+                        for (let i = 0; i < data.length; i++) {
+                            if (data[i].EmrType && +data[i].EmrType === 2) {
+                                this.secondaryInfo = data[i];
+                            }
+                        }
+                    }
+                }
+            },
+                err => {
+                    this.isLoading = false;
+                    this.messageService.add({ key: 'toastKey1', severity: 'error', summary: 'Failed to load secondary info', detail: '' });
+                },
+                () => {
+                    this.isLoading = false;
+                });
     }
-    catch (e) {
-      filename = 'myfile.pdf'
+
+    onDownloadClick() {
+        this.isLoading = true;
+        this.emergencyInfoService.getEmergencyInfoFileByApplicantId(this.storageService.getApplicantId)
+            .subscribe(response => {
+                if (response.status === 200) {
+                    const filename = this.getFileName(response)
+                    const downloadLink = document.createElement('a');
+                    downloadLink.href = window.URL.createObjectURL(response.body);
+                    downloadLink.setAttribute('download', filename.trim().replace('"', '').replace('"', ''));
+                    document.body.appendChild(downloadLink);
+                    downloadLink.click();
+                }
+            },
+                err => {
+                    this.isLoading = false;
+                    this.messageService.add({ key: 'toastKey1', severity: 'error', summary: 'Failed to get emergency info file', detail: '' });
+                },
+                () => {
+                    this.isLoading = false;
+                });
     }
-    return filename
-  }
+
+    getFileName(response: HttpResponse<Blob>) {
+        let filename: string;
+        try {
+            const contentDisposition = response.headers.get('content-disposition');
+            filename = contentDisposition.split(';')[1].split('filename')[1].split('=')[1].trim();
+        }
+        catch (e) {
+            filename = 'myfile.pdf'
+        }
+        return filename
+    }
 }
