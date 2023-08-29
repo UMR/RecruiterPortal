c43839cedb5333c05ae7384765c19566a1a37e67
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-29 21:00:06 +0600

    get zipcode in terms

--- RecruiterPortal.Client/src/app/applicant-details/terms-conditions/terms-conditions.service.ts
+++ RecruiterPortal.Client/src/app/applicant-details/terms-conditions/terms-conditions.service.ts
@@ -6,37 +6,33 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class TermsConditionsService {
 
-  private zipcodeCityStateURI: string = `${resourceServerUrl}/api/applicant-info/zipcode-city-state/`;
-  private getTermsConditionsByApplicantIdURI: string = `${resourceServerUrl}/api/terms-conditions/get-terms-conditions`;
-  private getTermsConditionsFileByApplicantIdURI: string = `${resourceServerUrl}/api/terms-conditions/get-terms-conditions-file`;
-  private saveTermsConditionsURI: string = `${resourceServerUrl}/api/terms-conditions/save-terms-conditions`;
+    private zipcodeCityStateURI: string = `${resourceServerUrl}/api/applicant-info/zipcode-city-state/`;
+    private getTermsConditionsByApplicantIdURI: string = `${resourceServerUrl}/api/terms-conditions/get-terms-conditions`;
+    private getTermsConditionsFileByApplicantIdURI: string = `${resourceServerUrl}/api/terms-conditions/get-terms-conditions-file`;
+    private saveTermsConditionsURI: string = `${resourceServerUrl}/api/terms-conditions/save-terms-conditions`;
 
-  constructor(private httpClient: HttpClient) {
+    constructor(private httpClient: HttpClient) {
 
-  }
-
-  getZipCodeCityStateByZipCode(zipCode: string = ""): Observable<HttpResponse<any>> {
-    if (zipCode != "") {
-      return this.httpClient.get(encodeURI(this.zipcodeCityStateURI + zipCode), { observe: 'response' });
     }
-    else {
-      return this.httpClient.get(encodeURI(this.zipcodeCityStateURI + "all"), { observe: 'response' });
+
+    getZipCodeCityStateByZipCode(zipCode: string = ""): Observable<HttpResponse<any>> {
+        const URI = `${resourceServerUrl}/api/applicant-info/zipcode-city-state?zipCode=${zipCode}`;
+        return this.httpClient.get(URI, { observe: 'response' });
     }
-  }
 
-  getTermsConditionsByApplicantId(applicantId: number): Observable<HttpResponse<any>> {
-    return this.httpClient.get(`${this.getTermsConditionsByApplicantIdURI}/${applicantId}`, { observe: 'response' });
-  }
+    getTermsConditionsByApplicantId(applicantId: number): Observable<HttpResponse<any>> {
+        return this.httpClient.get(`${this.getTermsConditionsByApplicantIdURI}/${applicantId}`, { observe: 'response' });
+    }
 
-  getTermsConditionsFileByApplicantId(applicantId: number): Observable<HttpResponse<any>> {
-    return this.httpClient.get(`${this.getTermsConditionsFileByApplicantIdURI}/${applicantId}`, { observe: 'response', responseType: 'blob' });
-  }
+    getTermsConditionsFileByApplicantId(applicantId: number): Observable<HttpResponse<any>> {
+        return this.httpClient.get(`${this.getTermsConditionsFileByApplicantIdURI}/${applicantId}`, { observe: 'response', responseType: 'blob' });
+    }
 
-  save(termsConditionsForm: any): Observable<HttpResponse<any>> {
-    return this.httpClient.post(this.saveTermsConditionsURI, termsConditionsForm, {
-      headers: new HttpHeaders()
-        .set('Content-Type', 'application/json'), observe: 'response', responseType: 'text'
-    });
-  }
+    save(termsConditionsForm: any): Observable<HttpResponse<any>> {
+        return this.httpClient.post(this.saveTermsConditionsURI, termsConditionsForm, {
+            headers: new HttpHeaders()
+                .set('Content-Type', 'application/json'), observe: 'response', responseType: 'text'
+        });
+    }
 
 }
