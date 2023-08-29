1233c0224ff709a471b62524e9717177e8e32376
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-29 18:13:07 +0600

    show primary info

--- RecruiterPortal.Client/src/app/applicant-details/emergency-info/primary/primary.component.ts
+++ RecruiterPortal.Client/src/app/applicant-details/emergency-info/primary/primary.component.ts
@@ -6,72 +6,72 @@ import { StorageService } from '../../../common/services/storage.service';
 import { HttpResponse } from '@angular/common/http';
 
 @Component({
-  selector: 'app-primary',
-  templateUrl: './primary.component.html',
-  styleUrls: ['./primary.component.css']
+    selector: 'app-primary',
+    templateUrl: './primary.component.html',
+    styleUrls: ['./primary.component.css']
 })
 export class PrimaryComponent implements OnInit {
-  public isLoading: boolean = false;  
-  public primaryInfo: EmergencyInfoModel[] = [];
+    public isLoading: boolean = false;
+    public primaryInfo: EmergencyInfoModel[] = [];
 
-  constructor(private messageService: MessageService,
-    private emergencyInfoService: EmergencyInfoService,
-    private storageService: StorageService,
-    private router: Router) { }
+    constructor(private messageService: MessageService,
+        private emergencyInfoService: EmergencyInfoService,
+        private storageService: StorageService,
+        private router: Router) { }
 
-  ngOnInit() {
-    this.isLoading = true;
-    this.emergencyInfoService.getEmergencyInfoByApplicantId(this.storageService.getApplicantId)
-      .subscribe(response => {        
-        const data = (response as any).Data;
-        if (data) {
-          for (let i = 0; i < data.length; i++) {
-            if (data[i].EmrType && +data[i].EmrType === 1) {              
-              this.primaryInfo = data[i];              
-            }
-          }
-        }
-      },
-        err => {
-          this.isLoading = false;
-          this.messageService.add({ key: 'toastKey1', severity: 'error', summary: 'Failed to load primary info', detail: '' });
-        },
-        () => {
-          this.isLoading = false;
-        });
-  }
-
-  onDownloadClick() {
-    this.isLoading = true;
-    this.emergencyInfoService.getEmergencyInfoFileByApplicantId(this.storageService.getApplicantId)
-      .subscribe(response => {        
-        if (response.status === 200) {
-          const filename = this.getFileName(response)
-          const downloadLink = document.createElement('a');
-          downloadLink.href = window.URL.createObjectURL(response.body);
-          downloadLink.setAttribute('download', filename.trim().replace('"', '').replace('"', ''));
-          document.body.appendChild(downloadLink);
-          downloadLink.click();
-        }
-      },
-        err => {
-          this.isLoading = false;
-          this.messageService.add({ key: 'toastKey1', severity: 'error', summary: 'Failed to get emergency info file', detail: '' });
-        },
-        () => {
-          this.isLoading = false;
-        });
-  }
-
-  getFileName(response: HttpResponse<Blob>) {
-    let filename: string;
-    try {
-      const contentDisposition = response.headers.get('content-disposition');
-      filename = contentDisposition.split(';')[1].split('filename')[1].split('=')[1].trim();
+    ngOnInit() {
+        this.isLoading = true;
+        this.emergencyInfoService.getEmergencyInfoByApplicantId(this.storageService.getApplicantId)
+            .subscribe(response => {
+                const data = response.body;
+                if (data && data.length > 0) {
+                    for (let i = 0; i < data.length; i++) {
+                        if (data[i].EmrType && +data[i].EmrType === 1) {
+                            this.primaryInfo = data[i];
+                        }
+                    }
+                }
+            },
+                err => {
+                    this.isLoading = false;
+                    this.messageService.add({ key: 'toastKey1', severity: 'error', summary: 'Failed to load primary info', detail: '' });
+                },
+                () => {
+                    this.isLoading = false;
+                });
     }
-    catch (e) {
-      filename = 'myfile.pdf'
+
+    onDownloadClick() {
+        this.isLoading = true;
+        this.emergencyInfoService.getEmergencyInfoFileByApplicantId(this.storageService.getApplicantId)
+            .subscribe(response => {
+                if (response.status === 200) {
+                    const filename = this.getFileName(response)
+                    const downloadLink = document.createElement('a');
+                    downloadLink.href = window.URL.createObjectURL(response.body);
+                    downloadLink.setAttribute('download', filename.trim().replace('"', '').replace('"', ''));
+                    document.body.appendChild(downloadLink);
+                    downloadLink.click();
+                }
+            },
+                err => {
+                    this.isLoading = false;
+                    this.messageService.add({ key: 'toastKey1', severity: 'error', summary: 'Failed to get emergency info file', detail: '' });
+                },
+                () => {
+                    this.isLoading = false;
+                });
     }
-    return filename
-  }
+
+    getFileName(response: HttpResponse<Blob>) {
+        let filename: string;
+        try {
+            const contentDisposition = response.headers.get('content-disposition');
+            filename = contentDisposition.split(';')[1].split('filename')[1].split('=')[1].trim();
+        }
+        catch (e) {
+            filename = 'myfile.pdf'
+        }
+        return filename
+    }
 }
