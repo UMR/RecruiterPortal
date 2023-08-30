6a8cdc4086c993264c9b584e5cac377138d09858
Sohel Rana <srana@ael-bd.com>
2023-08-30 18:26:05 +0600

    role based menu added

--- RecruiterPortal.Client/src/app/common/components/header/header.component.ts
+++ RecruiterPortal.Client/src/app/common/components/header/header.component.ts
@@ -5,64 +5,82 @@ import { UserProfileService } from '../../../user-profile/user-profile.service';
 import { AuthService } from '../../auth.service';
 
 @Component({
-  selector: 'applicant-portal-header',
-  templateUrl: './header.component.html',
-  styleUrls: ["./header.component.css"],
-  providers: [ AgencyProfileService]
+    selector: 'applicant-portal-header',
+    templateUrl: './header.component.html',
+    styleUrls: ["./header.component.css"],
+    providers: [AgencyProfileService]
 })
 export class HeaderComponent {
-  public isAgencyOwner: boolean = false;
-  public user: any = {};
-  public username: string = '';
-  @Input() isVisibleToggleButton: number;
-  @Output() onToggleButtonClicked = new EventEmitter<any>();
+    public isAgencyOwner: boolean = false;
+    public user: any = {};
+    public username: string = '';
+    @Input() isVisibleToggleButton: number;
+    @Output() onToggleButtonClicked = new EventEmitter<any>();
 
-  constructor(private authService: AuthService, private route: ActivatedRoute, private userProfileService: UserProfileService, public agencyProfileService: AgencyProfileService) {
+    public isRecruiter: boolean = false;
+    public isSupervisor: boolean = false;
+    public isManager: boolean = false;
+    public isAdministrator: boolean = false;
 
-  }
+    constructor(private authService: AuthService, private route: ActivatedRoute, private userProfileService: UserProfileService, public agencyProfileService: AgencyProfileService) {
 
-  ngOnInit() {
-    //this.agencyProfileService.isAgencyOwner()
-    //  .subscribe(response => {
-    //    if (response.status === 200) {
-    //      this.isAgencyOwner = response.body;
-    //    }
-    //  });
+    }
 
-    this.userProfileService.getUserRoles()
-        .subscribe(response => {
-            console.log(response);
-        if (response.status === 200) {          
-          this.userProfileService.setUsername(response.body.LastName + ' ' + response.body.FirstName);          
-        }
-      });    
+    ngOnInit() {
+        //this.agencyProfileService.isAgencyOwner()
+        //  .subscribe(response => {
+        //    if (response.status === 200) {
+        //      this.isAgencyOwner = response.body;
+        //    }
+        //  });
 
-    //this.userProfileService.selectedUsername$.subscribe(username => {
-    //  if (username) {
-    //    this.username = username;
-    //  }
-    //});
+        this.userProfileService.getUserRoles()
+            .subscribe(response => {
+                console.log(response);
+                if (response.status === 200 && response.body != null) {
+                    for (var i = 0; i < response.body.length; i++) {
+                        if (response.body[i].RoleKey == 'recruiter') {
+                            this.isRecruiter = true;
+                        }
+                        if (response.body[i].RoleKey == 'supervisor') {
+                            this.isSupervisor = true;
+                        }
+                        if (response.body[i].RoleKey == 'manager') {
+                            this.isManager = true;
+                        }
+                        if (response.body[i].RoleKey == 'administrator') {
+                            this.isAdministrator = true;
+                        }
+                    }
+                }
+            });
 
-    //if (this.route.snapshot.data['home']) {
-    //  this.userProfileService.setUser(this.route.snapshot.data['home'][0].body);
-    //  this.userProfileService.selectedUser$.subscribe(user => {
-    //    console.log(user);
-    //    this.fullName = user.LastName + user.FirstName;
-    //  });
-    //  this.isAgencyOwner = true;
+        //this.userProfileService.selectedUsername$.subscribe(username => {
+        //  if (username) {
+        //    this.username = username;
+        //  }
+        //});
 
-    //  //this.user = this.route.snapshot.data['home'][0].body;
-    //  //this.isAgencyOwner = this.route.snapshot.data['home'][1].body;
-    //  //this.fullName = this.user.LastName + this.user.FirstName;
-    //}
-  }
+        //if (this.route.snapshot.data['home']) {
+        //  this.userProfileService.setUser(this.route.snapshot.data['home'][0].body);
+        //  this.userProfileService.selectedUser$.subscribe(user => {
+        //    console.log(user);
+        //    this.fullName = user.LastName + user.FirstName;
+        //  });
+        //  this.isAgencyOwner = true;
 
-  logout() {
-    this.authService.logout();
-  }
+        //  //this.user = this.route.snapshot.data['home'][0].body;
+        //  //this.isAgencyOwner = this.route.snapshot.data['home'][1].body;
+        //  //this.fullName = this.user.LastName + this.user.FirstName;
+        //}
+    }
 
-  menuToggle(e) {
-    e.preventDefault();
-    this.onToggleButtonClicked.emit();
-  }
+    logout() {
+        this.authService.logout();
+    }
+
+    menuToggle(e) {
+        e.preventDefault();
+        this.onToggleButtonClicked.emit();
+    }
 }
