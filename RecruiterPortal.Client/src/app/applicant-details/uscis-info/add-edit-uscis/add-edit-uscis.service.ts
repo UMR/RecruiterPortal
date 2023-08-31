8a8836caea289584ff035bca39fde278992e44f2
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-31 12:49:54 +0600

    add zipcode script

--- RecruiterPortal.Client/src/app/applicant-details/uscis-info/add-edit-uscis/add-edit-uscis.service.ts
+++ RecruiterPortal.Client/src/app/applicant-details/uscis-info/add-edit-uscis/add-edit-uscis.service.ts
@@ -6,41 +6,34 @@ import { StorageService } from '../../../common/services/storage.service';
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class EditUSCISService {
 
-  private userUSCISURI: string = `${resourceServerUrl}/api/user-uscis`;
-  private getUserUSCISURI: string = `${resourceServerUrl}/api/user-uscis/get`;
-  private saveUserUSCISURI: string = `${resourceServerUrl}/api/user-uscis/save`;
-  private zipcodeCityStateURI: string = `${resourceServerUrl}/api/applicant-info/zipcode-city-state/`;
+    private userUSCISURI: string = `${resourceServerUrl}/api/user-uscis`;
+    private getUserUSCISURI: string = `${resourceServerUrl}/api/user-uscis/get`;
+    private saveUserUSCISURI: string = `${resourceServerUrl}/api/user-uscis/save`;    
 
-  constructor(private http: HttpClient, private storageService: StorageService) {
-  }
-
-  getUserUSCIS(): Observable<HttpResponse<any>> {
-    return this.http.get(this.getUserUSCISURI + '/' + this.storageService.getApplicantId, { observe: 'response' });
-  }
-
-  getZipCodeCityStateByZipCode(zipCode: string = ""): Observable<HttpResponse<any>> {
-    if (zipCode != "") {
-      return this.http.get(encodeURI(this.zipcodeCityStateURI + zipCode), { observe: 'response' });
+    constructor(private http: HttpClient, private storageService: StorageService) {
     }
-    else {
-      return this.http.get(encodeURI(this.zipcodeCityStateURI + "all"), { observe: 'response' });
+
+    getUserUSCIS(): Observable<HttpResponse<any>> {
+        return this.http.get(this.getUserUSCISURI + '/' + this.storageService.getApplicantId, { observe: 'response' });
     }
-  }
 
-  save(userUSCIS: any): Observable<HttpResponse<any>> {
-    //console.log(userUSCIS);
-    return this.http.post(this.saveUserUSCISURI, userUSCIS, {
-      headers: new HttpHeaders()
-        .set('Content-Type', 'application/json'), observe: 'response', responseType: 'text'
-    });
-  }
+    getZipCodeCityStateByZipCode(zipCode: string = ""): Observable<HttpResponse<any>> {
+        const URI = `${resourceServerUrl}/api/applicant-info/zipcode-city-state?zipCode=${zipCode}`;
+        return this.http.get(URI, { observe: 'response' });
+    }
 
+    save(userUSCIS: any): Observable<HttpResponse<any>> {        
+        return this.http.post(this.saveUserUSCISURI, userUSCIS, {
+            headers: new HttpHeaders()
+                .set('Content-Type', 'application/json'), observe: 'response', responseType: 'text'
+        });
+    }
 
-  deleteUSCISInfo(id: any) {
-    return this.http.delete(this.userUSCISURI + "/delete/" + id);
-  }
+    deleteUSCISInfo(id: any) {
+        return this.http.delete(this.userUSCISURI + "/delete/" + id);
+    }
 }
--- RecruiterPortal.DB/2023-08-23/02_AZ_01_ALTER_VIEW_View_LookUp_ZipCode.sql
+++ RecruiterPortal.DB/2023-08-23/02_AZ_01_ALTER_VIEW_View_LookUp_ZipCode.sql
@@ -1,1 +1,21 @@
-﻿
+﻿USE [UMRRecruitmentApplicant]
+GO
+
+/****** Object:  View [dbo].[View_LookUp_ZipCode]    Script Date: 8/31/2023 12:49:12 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER VIEW [dbo].[View_LookUp_ZipCode]
+AS
+SELECT DISTINCT S.StateCode, S.StateName, Z.ZipCode, Z.City, Z.StateAbbr, Z.County, Z.decommissioned, S.Description
+FROM Lookup_ZipCode AS Z INNER JOIN [State] AS S ON Z.StateAbbr = S.StateCode
+
+
+
+GO
+
+
