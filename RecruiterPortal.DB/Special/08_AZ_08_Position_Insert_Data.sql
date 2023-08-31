e2a7bae009678e784add00e2256014a621951094
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-08-31 14:44:04 +0600

    add script for position

---
+++ RecruiterPortal.DB/Special/08_AZ_08_Position_Insert_Data.sql
@@ -0,0 +1,12 @@
+﻿
+INSERT INTO [UMRRecruitmentApplicant].[dbo].[Position]
+           ([PositionName]
+           ,[CreatedBy]
+           ,[CreatedDate]
+           ,[UpdatedBy]
+           ,[UpdatedDate])
+    
+
+SELECT [PositionName],[CreatedBy] ,[CreatedDate] ,[UpdatedBy] ,[UpdatedDate]  FROM [UMRRecruitementDB_New].[dbo].[Position]
+
+
