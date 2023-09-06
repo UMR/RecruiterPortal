d36b1196ba8f7591bc021589cc635906ce769361
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-09-06 19:11:50 +0600

    add model and manager

---
+++ RecruiterPortal.DAL/Managers/JobManager.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Data.SqlClient;
+using RecruiterPortal.DAL.Repository;
+using RecruiterPortal.DAL.SqlModels;
+using System.Dynamic;
+
+namespace RecruiterPortal.DAL.Managers
+{
+    public class JobManager
+    {
+        public static int Insert(Job job)
+        {
+            string spName = "sp_InsertJob";
+            try
+            {
+                GenericRepository<Job> jobRepo = new GenericRepository<Job>();
+                SqlParameter[] sqlParameters = jobRepo.GetSqlParametersFromObject(job, spName, "@");
+                jobRepo.Insert(spName, sqlParameters);
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public static int Update(Job job)
+        {
+            string spName = "sp_UpdateJob";
+            try
+            {
+                GenericRepository<Job> jobRepo = new GenericRepository<Job>();
+                SqlParameter[] sqlParameters = jobRepo.GetSqlParametersFromObject(job, spName, "@");
+                return jobRepo.Update(spName, sqlParameters);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public static IEnumerable<Job> GetJobByAgencyId(long agencyId)
+        {
+            string spName = "sp_GetJobByAgencyId";
+            try
+            {
+                GenericRepository<Job> jobRepo = new GenericRepository<Job>();
+                dynamic expandoObject = new ExpandoObject();
+                expandoObject.AgencyId = agencyId;
+                SqlParameter[] sqlParameters = jobRepo.GetSqlParametersFromExpandoObject(expandoObject, spName, "@");
+                return jobRepo.GetAll(spName, sqlParameters);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static Job GetJobById(int jobId)
+        {
+            string spName = "sp_GetJobById";
+            try
+            {
+                GenericRepository<Job> jobRepo = new GenericRepository<Job>();
+                dynamic expandoObject = new ExpandoObject();
+                expandoObject.JobId = jobId;
+                SqlParameter[] sqlParameters = jobRepo.GetSqlParametersFromExpandoObject(expandoObject, spName, "@");
+                return jobRepo.GetOne(spName, sqlParameters);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ RecruiterPortal.DAL/Models/JobModel.cs
@@ -0,0 +1,26 @@
+﻿namespace RecruiterPortal.DAL.Models;
+
+public class JobModel
+{
+    public int JobId { get; set; }
+
+    public bool? Status { get; set; }
+
+    public string JobTitle { get; set; }
+
+    public string JobDescription { get; set; }
+
+    public int? PositionId { get; set; }
+
+    public int? InstituteId { get; set; }
+
+    public long AgencyId { get; set; }
+
+    public int? CreatedBy { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    public int? UpdatedBy { get; set; }
+
+    public DateTime? UpdatedDate { get; set; }   
+}
