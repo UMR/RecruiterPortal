c9d284e5f4fc3b4c49f2479e72c345d57404dcb4
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-09-06 21:07:36 +0600

    add script

---
+++ RecruiterPortal.DB/2023-09-06/02_AZ_02_CREATE_PROCEDURE_sp_InsertJob.sql
@@ -0,0 +1,39 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+
+CREATE PROCEDURE [dbo].[sp_InsertJob]	
+	@Status [bit] NULL,
+	@JobTitle [nvarchar](500) NULL,
+	@JobDescription [nvarchar](max) NULL,
+	@PositionId [int] NULL,
+	@InstituteId [int] NULL,
+	@AgencyId [bigint],
+	@CreatedBy [int] NULL,
+	@CreatedDate [datetime] NULL	
+AS
+BEGIN
+
+INSERT INTO [dbo].[Job]
+           ([Status]
+           ,[JobTitle]
+           ,[JobDescription]
+           ,[PositionId]
+           ,[InstituteId]
+           ,[AgencyId]
+           ,[CreatedBy]
+           ,[CreatedDate]
+           )
+     VALUES
+           (
+				@Status
+			   ,@JobTitle
+			   ,@JobDescription
+			   ,@PositionId
+			   ,@InstituteId
+			   ,@AgencyId
+			   ,@CreatedBy
+			   ,@CreatedDate
+		   )
+END
+
+
