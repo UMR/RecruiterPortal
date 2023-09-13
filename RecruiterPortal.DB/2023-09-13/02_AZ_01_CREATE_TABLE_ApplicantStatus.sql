6487564863715c8dff7053c8e4933188e2a005c5
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-09-13 20:58:27 +0600

    add script

---
+++ RecruiterPortal.DB/2023-09-13/02_AZ_01_CREATE_TABLE_ApplicantStatus.sql
@@ -0,0 +1,71 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+
+/****** Object:  Table [dbo].[ApplicantStatus]    Script Date: 9/13/2023 8:56:43 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ApplicantStatus](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[ApplicantId] [bigint] NOT NULL,
+	[PositionId] [int] NULL,
+	[InstitutionId] [int] NULL,
+	[Status] [tinyint] NOT NULL,
+	[Date] [datetime] NULL,
+	[TotalFee] [float] NULL,
+	[NetFee] [float] NULL,
+	[RefFee] [float] NULL,
+	[CurrentSalary] [decimal](10, 2) NULL,
+	[ExpectedSalary] [decimal](10, 2) NULL,
+	[CreatedBy] [int] NULL,
+	[CreatedDate] [datetime] NULL,
+	[UpdatedBy] [int] NULL,
+	[UpdatedDate] [datetime] NULL,
+	[ProfileStatus] [tinyint] NULL,
+	[Shift] [varchar](50) NULL,
+ CONSTRAINT [PK_ApplicantInstitutionStatus] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[ApplicantStatus]  WITH CHECK ADD  CONSTRAINT [FK_ApplicantStatus_Institution] FOREIGN KEY([InstitutionId])
+REFERENCES [dbo].[Institution] ([Id])
+GO
+
+ALTER TABLE [dbo].[ApplicantStatus] CHECK CONSTRAINT [FK_ApplicantStatus_Institution]
+GO
+
+ALTER TABLE [dbo].[ApplicantStatus]  WITH CHECK ADD  CONSTRAINT [FK_ApplicantStatus_Position] FOREIGN KEY([PositionId])
+REFERENCES [dbo].[Position] ([Id])
+GO
+
+ALTER TABLE [dbo].[ApplicantStatus] CHECK CONSTRAINT [FK_ApplicantStatus_Position]
+GO
+
+ALTER TABLE [dbo].[ApplicantStatus]  WITH CHECK ADD  CONSTRAINT [FK_ApplicantStatus_Recruiter] FOREIGN KEY([CreatedBy])
+REFERENCES [dbo].[Recruiter] ([RecruiterID])
+GO
+
+ALTER TABLE [dbo].[ApplicantStatus] CHECK CONSTRAINT [FK_ApplicantStatus_Recruiter]
+GO
+
+ALTER TABLE [dbo].[ApplicantStatus]  WITH CHECK ADD  CONSTRAINT [FK_ApplicantStatus_Recruiter1] FOREIGN KEY([UpdatedBy])
+REFERENCES [dbo].[Recruiter] ([RecruiterID])
+GO
+
+ALTER TABLE [dbo].[ApplicantStatus] CHECK CONSTRAINT [FK_ApplicantStatus_Recruiter1]
+GO
+
+ALTER TABLE [dbo].[ApplicantStatus]  WITH CHECK ADD  CONSTRAINT [FK_ApplicantStatus_User] FOREIGN KEY([ApplicantId])
+REFERENCES [dbo].[User] ([UserID])
+GO
+
+ALTER TABLE [dbo].[ApplicantStatus] CHECK CONSTRAINT [FK_ApplicantStatus_User]
+GO
+
+
