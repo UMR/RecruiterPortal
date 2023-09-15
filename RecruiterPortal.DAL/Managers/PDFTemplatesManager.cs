4318d6646170afb65bd85000a972809dd0c92566
Sohel Rana <srana@ael-bd.com>
2023-09-15 18:08:46 +0600

    insert data

--- RecruiterPortal.DAL/Managers/PDFTemplatesManager.cs
+++ RecruiterPortal.DAL/Managers/PDFTemplatesManager.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using RecruiterPortal.DAL.Repository;
+using RecruiterPortal.DAL.SqlModels;
 using System.Data;
 using System.Dynamic;
 
@@ -9,23 +10,23 @@ namespace RecruiterPortalDAL.Managers
     {
         public static DataTable GetPDFTemplatesFileType(string fileTypeCode)
         {
-            //string spName = "sp_GetPDFFileTemplateFileType";
-            //dynamic expandoObject = new ExpandoObject();
-            //expandoObject.FileTypeCode = fileTypeCode;
-            //GenericRepository<PDFTemplate> userRepo = new GenericRepository<PDFTemplate>();
-            //SqlParameter[] sqlParameters = userRepo.GetSqlParametersFromExpandoObject(expandoObject, spName);
-            //DataTable pdfTemplate = null;
-            //try
-            //{
-            //    pdfTemplate = userRepo.LoadDataTable(spName, sqlParameters);
-            //}
-            //catch (Exception ex)
-            //{
-            //    throw ex;
-            //}
-            //return pdfTemplate;
+            string spName = "sp_GetPDFFileTemplateFileType";
+            dynamic expandoObject = new ExpandoObject();
+            expandoObject.FileTypeCode = fileTypeCode;
+            GenericRepository<Pdftemplate> userRepo = new GenericRepository<Pdftemplate>();
+            SqlParameter[] sqlParameters = userRepo.GetSqlParametersFromExpandoObject(expandoObject, spName);
+            DataTable pdfTemplate = null;
+            try
+            {
+                pdfTemplate = userRepo.LoadDataTable(spName, sqlParameters);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return pdfTemplate;
 
-            return new DataTable();
+            //return new DataTable();
         }
     }
 }
---
+++ RecruiterPortal.DB/Special/09_SR_01_INSERT_File_Data.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO [UMRRecruitmentApplicant].[dbo].[PDFTemplates]
+           ([FileTypeCode]
+           ,[FileData]
+           ,[FIleName])
+   SELECT [FileTypeCode],FileData,[FIleName]  FROM [UMRRecruitementDB_New].[dbo].[PDFTemplates]
