852b7025500548c465e923740ad9a3d44ab851a0
Sohel Rana <srana@ael-bd.com>
2023-09-18 17:28:04 +0600

    alter SP

---
+++ RecruiterPortal.DB/2023-09-18/01_SR_01_ALTER_sp_GetPDFFileTemplateFileType.sql
@@ -0,0 +1,14 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_GetPDFFileTemplateFileType]    Script Date: 9/18/2023 5:24:55 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[sp_GetPDFFileTemplateFileType]
+	 @FileTypeCode nvarchar(500)
+AS
+	SET NOCOUNT ON;
+	SELECT * FROM [dbo].[PDFTemplates]
+	WHERE FileTypeCode=@FileTypeCode
