b84aea15418804ec70fe18e9d688f0c38aa6c022
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-09-25 14:43:52 +0600

    get mail types in Template in client side

--- RecruiterPortal.Client/src/app/timecards/mail-settings/mail-settings.component.ts
+++ RecruiterPortal.Client/src/app/timecards/mail-settings/mail-settings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MessageService, ConfirmationService } from 'primeng/api';
+import { MailSettingsService } from './mail-settings.service';
 
 @Component({
     selector: 'app-mail-settings',
@@ -11,13 +12,20 @@ export class MailSettingsComponent implements OnInit {
 
     public isLoading: boolean = false;
     public showMailTemplateType: boolean = false;
-    public formGroup: FormGroup;    
+    public formGroup: FormGroup;
+    public mailTemplateTypes: any[] = [];
 
-    constructor(private fb: FormBuilder, private messageService: MessageService, private confirmationService: ConfirmationService) {        
+    constructor(private fb: FormBuilder, private messageService: MessageService,
+        private confirmationService: ConfirmationService, private mailSettingsService: MailSettingsService) {        
     }
 
     ngOnInit() {
         this.createFormGroup();
+        this.getMailTemplateTypesByRecruiterId();
+    }
+
+    getMailTemplateTypesByRecruiterId() {
+        this.mailSettingsService.getMailTemplateTypesByRecruiterId().subscribe(res => this.mailTemplateTypes = res.body);
     }
 
     createFormGroup() {
