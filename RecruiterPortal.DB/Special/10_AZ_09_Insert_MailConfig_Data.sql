858186da314b12807c4fe2b669573bb900ac3c43
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-09-25 16:37:34 +0600

    script added

---
+++ RecruiterPortal.DB/Special/10_AZ_09_Insert_MailConfig_Data.sql
@@ -0,0 +1,25 @@
+﻿
+INSERT INTO [UMRRecruitmentApplicant].[dbo].[RecruiterMailConfig] 
+			([ProfileName]
+           ,[RecruiterId]
+           ,[Email]
+           ,[GoogleRefreshToken]
+           ,[GoogleDriveFolderId]
+           ,[CreatedBy]
+           ,[CreatedDate]
+           ,[UpdatedBy]
+           ,[UpdatedDate]
+           ,[IsGoogleApiError])         
+
+SELECT [ProfileName]
+      ,1      
+      ,[POP3UserName] AS Email      
+      ,[GoogleRefreshToken]
+      ,[GoogleDriveFolderId]
+      ,[CreatedBy]
+      ,[CreatedDate]
+      ,[UpdatedBy]
+      ,[UpdatedDate]
+      ,[IsGoogleApiError]
+  FROM [UMRRecruitementDB_New].[dbo].[MailConfiguration]
+
