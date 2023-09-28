c29da72a79600d83748b458a5003fad601b3c549
Sohel Rana <srana@ael-bd.com>
2023-09-28 16:59:10 +0600

    all agency

---
+++ RecruiterPortal.DB/2023-09-28/02_SR_01_ALTER_SP_All_GET_Agency.sql
@@ -0,0 +1,17 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_All_GET_Agency]    Script Date: 9/28/2023 2:32:15 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[SP_All_GET_Agency]  
+AS 
+BEGIN      
+	SELECT * FROM [Agency] ;
+END
