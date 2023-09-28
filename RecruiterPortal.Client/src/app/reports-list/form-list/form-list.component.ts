263605a34b750dce1fede804ba8f34f7c6e93f72
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-09-28 19:54:20 +0600

    create form

--- RecruiterPortal.Client/src/app/reports-list/form-list/form-list.component.ts
+++ RecruiterPortal.Client/src/app/reports-list/form-list/form-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { LazyLoadEvent } from 'primeng/api';
+import { LazyLoadEvent, MessageService, ConfirmationService } from 'primeng/api';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-form-list',
@@ -17,14 +18,55 @@ export class FormListComponent implements OnInit {
     private skip: number;
     private pageNumber: number;
 
-    constructor() { }
+    public showDialog: boolean = false;
+    public addEditTitle: string;
+    public formGroup: FormGroup;
+
+    constructor(private fb: FormBuilder, private messageService: MessageService, private confirmationService: ConfirmationService) {
+        this.addEditTitle = "Add";
+    }
 
     ngOnInit() {
+        this.createFormGroup();
+    }
+
+    createFormGroup() {
+        this.formGroup = this.fb.group({
+            title: ['', Validators.compose([Validators.required, Validators.maxLength(200)])],
+            isRequired: [''],
+            isAdministrative: [''],
+            isActive: ['', Validators.compose([Validators.required])],            
+        });
     }
 
     onLazyLoad(event: LazyLoadEvent) {
     }
 
-    onDeleteClick() {
+    onNew() {
+        //this.selectedJobId = 0;
+        //this.selectedJob = null;
+        //this.addEditTitle = "Add";
+        this.showDialog = true;
+        //this.setDefaultFields();
+    }
+
+    onEdit(job) {
+        //this.selectedJobId = job.JobId;
+        //this.getJobsById(this.selectedJobId);
+        //this.jobDialog = true;
+    }
+
+    onClear() {
+        this.formGroup.reset();
+    }
+
+    onHide() {
+        this.showDialog = false;
+    }
+
+    onSave() {
+    }
+
+    onDelete() {
     }
 }
--- RecruiterPortal.Client/src/app/reports-list/form-list/form-list.module.ts
+++ RecruiterPortal.Client/src/app/reports-list/form-list/form-list.module.ts
@@ -5,6 +5,11 @@ import { FormListRoutingModule } from './form-list-routing.module';
 import { FormListComponent } from './form-list.component';
 import { SharedModule } from '../../common/shared.module';
 import { TableModule } from 'primeng/components/table/table';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmDialogModule } from 'primeng/primeng';
 
 
 @NgModule({
@@ -13,7 +18,13 @@ import { TableModule } from 'primeng/components/table/table';
         CommonModule,
         FormListRoutingModule,
         SharedModule,
-        TableModule
-    ]
+        TableModule,
+        ToastModule,
+        FormsModule,
+        ReactiveFormsModule,
+        DialogModule,
+        ConfirmDialogModule
+    ],
+    providers: [ConfirmationService, MessageService]
 })
 export class FormListModule { }
