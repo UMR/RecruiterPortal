49c57a86dfc0587c78961df298bb7dedbc3b9cf4
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-09-28 14:02:36 +0600

    add script

---
+++ RecruiterPortal.DB/2023-09-28/01_AZ_01_CREATE_TABLE_OfficialFiles.sql
@@ -0,0 +1,32 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[OfficialFiles](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FileName] [nvarchar](200) NOT NULL,
+	[FileData] [varbinary](max) NOT NULL,
+	[Title] [nvarchar](200) NULL,
+	[IsRequired] [bit] NOT NULL,
+	[IsAdministrative] [bit] NOT NULL,		
+	[IsActive] [bit] NOT NULL,
+	[CreatedBy] [int] NOT NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[UpdatedBy] [int] NULL,
+	[UpdatedDate] [datetime] NULL,	
+	[AgencyId] [bigint] NOT NULL
+ CONSTRAINT [PK_OfficialFiles] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+
+
