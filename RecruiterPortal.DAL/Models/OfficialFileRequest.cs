2a75304438e38a1f64fbdb3bfbbbee0219706536
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-09-28 14:24:33 +0600

    add manager and models

---
+++ RecruiterPortal.DAL/Models/OfficialFileRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace RecruiterPortal.DAL.Models
+{
+    public class OfficialFileRequest
+    {
+        public int Id { get; set; }
+
+        public string FileName { get; set; }
+
+        public byte[] FileData { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsRequired { get; set; }
+
+        public bool IsAdministrative { get; set; }
+
+        public bool IsActive { get; set; }
+        
+    }
+}
