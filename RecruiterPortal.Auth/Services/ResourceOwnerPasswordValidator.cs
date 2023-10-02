0b73235cf01dea940f1ca1400bf6cf469eedde66
Sohel Rana <srana@ael-bd.com>
2023-10-02 15:46:22 +0600

    Update ResourceOwnerPasswordValidator.cs

--- RecruiterPortal.Auth/Services/ResourceOwnerPasswordValidator.cs
+++ RecruiterPortal.Auth/Services/ResourceOwnerPasswordValidator.cs
@@ -27,7 +27,7 @@ namespace ApplicantPortalAPI.AuthorizationServer.Services
                 {
                     var recruiter = RecruiterManager.GetRecruiterByLoginid(context.UserName);
                     context.Result = new GrantValidationResult(context.UserName, OidcConstants.AuthenticationMethods.Password);
-                    var v = RecruiterManager.InsertRecruiterEntry(recruiter.RecruiterId);
+                    var insert = RecruiterManager.InsertRecruiterEntry(recruiter.RecruiterId);
                     _logger.LogInformation($"Authentication Pass For User {context.UserName}");
                 }
             }
