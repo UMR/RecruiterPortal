043e86d1bf84aeb03d9e6ab78a5f638e3da82db3
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-10-11 20:39:59 +0600

    get mail configuration

--- RecruiterPortal.DAL/Managers/MailConfigurationManager.cs
+++ RecruiterPortal.DAL/Managers/MailConfigurationManager.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Data.SqlClient;
-using RecruiterPortal.DAL.Models;
+﻿using RecruiterPortal.DAL.Models;
 using RecruiterPortal.DAL.Repository;
 using RecruiterPortal.DAL.SqlModels;
 
@@ -7,66 +6,13 @@ namespace RecruiterPortalDAL.Managers
 {
     public interface IMailConfigurationManager
     {
-        Task<int> Create(MailConfigurationRequest request, int recruiterId);
-        ViewMailConfiguration GetEailConfigByAdress(string p_POP3UserName);
+        Task<int> Create(MailConfigurationRequest request, int recruiterId);        
         Task<bool?> Update(MailConfigurationRequest request, int recruiterId);
+        Task<bool?> Delete(int id);
     }
 
     public class MailConfigurationManager : IMailConfigurationManager
     {
-        public const string POP3USERNAME = "trahman@ael-bd.com";
-
-        public ViewMailConfiguration GetEailConfigByAdress(string p_POP3UserName)
-        {
-            string spName = "sp_GetMailConfigByPOP3UserName";
-            SqlParameter[] sqlParameters = new GenericRepository<ViewMailConfiguration>().GetSqlParametersFromStoredProcedure(spName);
-
-            foreach (SqlParameter sqlParameter in sqlParameters)
-            {
-                if ("@" + nameof(p_POP3UserName) == sqlParameter.ParameterName)
-                {
-                    sqlParameter.Value = p_POP3UserName;
-                }
-            }
-            ViewMailConfiguration mailVerfication = null;
-            try
-            {
-
-                mailVerfication = new GenericRepository<ViewMailConfiguration>().GetOne(spName, sqlParameters);
-
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-
-            return mailVerfication;
-        }
-
-        private RecruiterMailConfig MapMailConfigCreateRequest(MailConfigurationRequest request, int recruiterId)
-        {
-            RecruiterMailConfig mailConfig = new RecruiterMailConfig();
-            mailConfig.RecruiterId = recruiterId;
-            mailConfig.ProfileName = request.ProfileName;
-            mailConfig.Email = request.EmailAddress;
-            mailConfig.GoogleRefreshToken = request.GoogleRefreshToken;
-            mailConfig.CreatedBy = recruiterId;
-            mailConfig.CreatedDate = DateTime.Now;
-            return mailConfig;
-        }
-
-        private RecruiterMailConfig MapMailConfigUpdateRequest(MailConfigurationRequest request, RecruiterMailConfig mailConfig, int recruiterId)
-        {
-            mailConfig.Id = request.Id;
-            mailConfig.RecruiterId = recruiterId;
-            mailConfig.ProfileName = request.ProfileName;
-            mailConfig.Email = request.EmailAddress;
-            mailConfig.GoogleRefreshToken = request.GoogleRefreshToken;
-            mailConfig.UpdatedBy = recruiterId;
-            mailConfig.UpdatedDate = DateTime.Now;
-            return mailConfig;
-        }
-
         public async Task<int> Create(MailConfigurationRequest request, int recruiterId)
         {
             try
@@ -100,6 +46,105 @@ namespace RecruiterPortalDAL.Managers
             {
                 throw new Exception(ex.Message);
             }
+        }
+
+        public async Task<bool?> Delete(int id)
+        {
+            try
+            {
+                GenericRepository<RecruiterMailConfig> repository = new GenericRepository<RecruiterMailConfig>();
+                RecruiterMailConfig mailConfig = await repository.GetByIdAsync(s => s.Id == id);
+                if (mailConfig != null)
+                {
+                    return await repository.DeleteAsync(mailConfig) > 0 ? true : false; ;
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static async Task<MailConfigurationResponse> GetMailConfigById(int id)
+        {
+            try
+            {
+                MailConfigurationResponse mailConfiguration = null;
+                GenericRepository<RecruiterMailConfig> repository = new GenericRepository<RecruiterMailConfig>();
+                var mailConfigurationFromDb = await repository.GetByIdAsync(m => m.Id == id);
+                if (mailConfigurationFromDb != null)
+                {
+                    mailConfiguration = MapMailConfigurationResponse(mailConfigurationFromDb);
+                }
+
+                return mailConfiguration;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public static async Task<List<MailConfigurationResponse>> GetMailTemplateTypesByRecruiterId(int recruiterId)
+        {
+            try
+            {
+                List<MailConfigurationResponse> mailTemplateTypes = new List<MailConfigurationResponse>();
+                GenericRepository<RecruiterMailConfig> repository = new GenericRepository<RecruiterMailConfig>();
+                var mailConfigurationFromDb = await repository.GetAllAsync(m => m.RecruiterId == recruiterId);
+                if (mailConfigurationFromDb != null)
+                {
+                    foreach (var mailTemplateTypeFromDb in mailConfigurationFromDb)
+                    {
+                        MailConfigurationResponse mailConfiguration = MapMailConfigurationResponse(mailTemplateTypeFromDb);
+                        mailTemplateTypes.Add(mailConfiguration);
+                    }
+                }
+
+                return mailTemplateTypes;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private static MailConfigurationResponse MapMailConfigurationResponse(RecruiterMailConfig request)
+        {
+            MailConfigurationResponse response = new MailConfigurationResponse();
+            response.Id = request.Id;
+            response.RecruiterId = request.RecruiterId;
+            response.ProfileName = request.ProfileName;
+            response.EmailAddress = request.Email;
+            response.GoogleRefreshToken = request.GoogleRefreshToken;
+            response.IsGoogleApiError = response.IsGoogleApiError;
+            return response;
+        }
+
+        private RecruiterMailConfig MapMailConfigCreateRequest(MailConfigurationRequest request, int recruiterId)
+        {
+            RecruiterMailConfig mailConfig = new RecruiterMailConfig();
+            mailConfig.RecruiterId = recruiterId;
+            mailConfig.ProfileName = request.ProfileName;
+            mailConfig.Email = request.EmailAddress;
+            mailConfig.GoogleRefreshToken = request.GoogleRefreshToken;
+            mailConfig.CreatedBy = recruiterId;
+            mailConfig.CreatedDate = DateTime.Now;
+            return mailConfig;
+        }
+
+        private RecruiterMailConfig MapMailConfigUpdateRequest(MailConfigurationRequest request, RecruiterMailConfig mailConfig, int recruiterId)
+        {
+            mailConfig.Id = request.Id;
+            mailConfig.RecruiterId = recruiterId;
+            mailConfig.ProfileName = request.ProfileName;
+            mailConfig.Email = request.EmailAddress;
+            mailConfig.GoogleRefreshToken = request.GoogleRefreshToken;
+            mailConfig.UpdatedBy = recruiterId;
+            mailConfig.UpdatedDate = DateTime.Now;
+            return mailConfig;
         }
     }
 }
---
+++ RecruiterPortal.DAL/Models/MailConfigurationResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace RecruiterPortal.DAL.Models
+{
+    public class MailConfigurationResponse
+    {
+        public int Id { get; set; }
+        public int RecruiterId { get; set; }
+        public string ProfileName { get; set; }
+        public string EmailAddress { get; set; }
+        public string GoogleRefreshToken { get; set; }
+        public string Code { get; set; }
+        public bool IsGoogleApiError { get; set; }
+    }
+}
