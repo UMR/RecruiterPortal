c0b6bcddb0875ec088ef198cbfbd3f681749d0ca
Sohel Rana <srana@ael-bd.com>
2023-10-11 18:32:59 +0600

    Update app.component.ts

--- RecruiterPortal.Client/src/app/app.component.ts
+++ RecruiterPortal.Client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Subject } from 'rxjs';
+import { AuthService } from './common/auth.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
+    private userActivity: any;
+    private userInactive: Subject<any> = new Subject();
+
+    constructor(private authService: AuthService) {
+        this.setTimeout();
+        this.userInactive.subscribe(() => this.authService.logout());
+    }
+
+    setTimeout() {
+        this.userActivity = setTimeout(() => this.userInactive.next(undefined), 10 * 60 * 1000);
+    }
+
+    @HostListener('window:mousemove') refreshUserState() {
+        clearTimeout(this.userActivity);
+        this.setTimeout();
+    }
 }
