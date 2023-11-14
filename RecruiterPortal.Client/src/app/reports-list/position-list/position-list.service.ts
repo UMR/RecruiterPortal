e26c0103f30352085ea859973aa4b41bea352021
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-11-14 18:41:55 +0600

    write angular service for position

---
+++ RecruiterPortal.Client/src/app/reports-list/position-list/position-list.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpResponse, HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { resourceServerUrl } from '../../common/constants/auth-keys';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PositionListService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    getPositions(page: number, pageSize: number): Observable<HttpResponse<any>> {
+        return this.http.get(`${resourceServerUrl}/api/position/get-all?page=${page}&pageSize=${pageSize}`, { observe: 'response' });
+    }
+
+    getPositionById(id: any): Observable<HttpResponse<any>> {
+        return this.http.get(`${resourceServerUrl}/api/position/get-by-id/${id}`, { observe: 'response' });
+    }
+
+    savePosition(position: any): Observable<HttpResponse<any>> {
+        return this.http.post(`${resourceServerUrl}/api/position/save`, position, {
+            headers: new HttpHeaders()
+                .set('Content-Type', 'application/json'), observe: 'response', responseType: 'text'
+        });
+    }
+
+    updatePosition(position: any): Observable<HttpResponse<any>> {
+        return this.http.put(`${resourceServerUrl}/api/position/update`, position, {
+            headers: new HttpHeaders()
+                .set('Content-Type', 'application/json'), observe: 'response', responseType: 'text'
+        });
+    }
+
+    deletePosition(id): Observable<HttpResponse<any>> {
+        return this.http.delete(`${resourceServerUrl}/api/position/delete/${id}`, {
+            headers: new HttpHeaders()
+                .set('Content-Type', 'application/json'), observe: 'response', responseType: 'text'
+        });
+    }
+    
+}
