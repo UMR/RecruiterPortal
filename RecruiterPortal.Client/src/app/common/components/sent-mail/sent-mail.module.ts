14f31dab2752dfa39d171b8e7c991e4a49fa2932
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-11-28 21:09:05 +0600

    get mail config and mail type and mail des

--- RecruiterPortal.Client/src/app/common/components/sent-mail/sent-mail.module.ts
+++ RecruiterPortal.Client/src/app/common/components/sent-mail/sent-mail.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../../common/shared.module';
 import { LoadingImageModule } from '../../../common/loading-image.module';
 import { ToastModule } from 'primeng/toast';
-import { ConfirmDialogModule, AutoCompleteModule, ConfirmationService, MessageService } from 'primeng/primeng';
+import { ConfirmDialogModule, AutoCompleteModule, ConfirmationService, MessageService, EditorModule } from 'primeng/primeng';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ChipsModule } from 'primeng/chips';
 
 import { SentMailService } from './sent-mail.service';
 import { SentMailComponent } from './sent-mail.component';
+import { MailTemplateService } from '../../../timecards/mail-template-type/mail-template-type.service';
 
 
 @NgModule({
@@ -23,9 +24,10 @@ import { SentMailComponent } from './sent-mail.component';
         FormsModule,
         ReactiveFormsModule,
         AutoCompleteModule,
-        ChipsModule
+        ChipsModule,
+        EditorModule
     ],
-    providers: [ConfirmationService, MessageService, SentMailService]
+    providers: [ConfirmationService, MessageService, SentMailService, MailTemplateService]
 })
 export class SentMailModule {
 
