53382be888cb7a7d5fcd8ee48089e858ad6664a3
Sohel Rana <srana@ael-bd.com>
2023-12-04 14:41:57 +0600

    sms log insert api

--- RecruiterPortal.DAL/Managers/SMSLogManager.cs
+++ RecruiterPortal.DAL/Managers/SMSLogManager.cs
@@ -1,7 +1,37 @@
-﻿namespace RecruiterPortalDAL.Managers
+﻿using RecruiterPortal.DAL.Models;
+using RecruiterPortal.DAL.Repository;
+using RecruiterPortal.DAL.SqlModels;
+
+namespace RecruiterPortalDAL.Managers
 {
     public class SMSLogManager
     {
+        public static async Task<int> Insert(SMSLogModel request, int recruiterId)
+        {
+            try
+            {
+                GenericRepository<Smslog> repository = new GenericRepository<Smslog>();
+                Smslog sms = MapObjectRequest(request, recruiterId);
+                Smslog createdJob = await repository.SaveAsync(sms);
+                return createdJob.Id;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        private static Smslog MapObjectRequest(SMSLogModel request, int recruiterId)
+        {
+            Smslog smslog = new Smslog();
+            smslog.Id = request.Id;
+            smslog.ToNumber = request.ToNumber;
+            smslog.FromNumber = request.FromNumber;
+            smslog.Smsbody = request.Smsbody;
+            smslog.SendTime = request.SendTime;
+            smslog.CreatedDate = DateTime.Now;
+            smslog.CreatedBy = recruiterId;
 
+            return smslog;
+        }
     }
 }
---
+++ RecruiterPortal.DAL/Models/SMSLogModel.cs
@@ -0,0 +1,24 @@
+﻿namespace RecruiterPortal.DAL.Models
+{
+    public class SMSLogModel
+    {
+        public int Id { get; set; }
+
+        public DateTime SendTime { get; set; }
+
+        public string FromNumber { get; set; }
+
+        public string ToNumber { get; set; }
+
+        public string Smsbody { get; set; }
+
+        public int? CreatedBy { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public int? UpdatedBy { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+    }
+}
