f03ae295d12ca6c528fec751163a7aab4229352a
Md. Atikur Zaman <arzaman@ael-bd.com>
2023-12-05 16:25:28 +0600

    add script

---
+++ RecruiterPortal.DB/2023-12-05/01_AZ_01_CREATE_PROCEDURE_GetAllEmailByFilter.sql
@@ -0,0 +1,26 @@
+﻿USE [UMRRecruitmentApplicant]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_GetAllEmailByFilter]    Script Date: 12/5/2023 4:23:19 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[sp_GetAllEmailByFilter] 	
+	@LastName NVARCHAR(100) = NULL,
+	@FirstName NVARCHAR(100) = NULL,
+	@Email NVARCHAR(100) = NULL,
+	@IsVerified bit = NULL,
+	@CurrentRecruiterId INT = NULL	
+AS
+BEGIN
+
+SELECT App.Email FROM [UMRRecruitmentApplicant].[dbo].[User] As App
+	WHERE  App.AgencyId IN (SELECT AgencyID FROM [dbo].Recruiter WHERE RecruiterID=@CurrentRecruiterId)
+	AND (App.[First_Name] LIKE '%'+ @FirstName +'%')
+	AND (App.[Last_Name] LIKE '%'+ @LastName +'%' )
+	AND (App.Email LIKE '%'+ @Email +'%' )
+	AND ([IsVerified] =@IsVerified )
+	ORDER BY App.CreatedDate
+  
+END
