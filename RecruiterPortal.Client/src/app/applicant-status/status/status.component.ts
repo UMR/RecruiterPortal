e9d03dd26c707126696053fd2bae3cb752720c87
Sohel Rana <srana@ael-bd.com>
2024-02-05 12:43:15 +0600

    Update status.component.ts

--- RecruiterPortal.Client/src/app/applicant-status/status/status.component.ts
+++ RecruiterPortal.Client/src/app/applicant-status/status/status.component.ts
@@ -35,6 +35,8 @@ export class StatusComponent implements OnInit {
             positionId: [''],
             institution: [''],
             instituteId: [''],
+            resume:[''],
+            notes:[],
             currentSalary: [''],
             expectedSalary:['']
         });
